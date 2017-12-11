d87ef39e1e8ec9e236d1b3a83426286bf97c7846
duff.brian21@gmail.com <duff.brian21@gmail.com>
2017-12-10 23:51:08 -0800

    added html outline with css

---
+++ assets/javascript/script.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBmMuEWylCvmnDq9fJnlSqBCou_rZtoJt8",
+    authDomain: "train-schedule-1766e.firebaseapp.com",
+    databaseURL: "https://train-schedule-1766e.firebaseio.com",
+    projectId: "train-schedule-1766e",
+    storageBucket: "",
+    messagingSenderId: "693426029702"
+  };
+  firebase.initializeApp(config);
+
+  
